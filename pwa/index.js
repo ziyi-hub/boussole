40194ec2f10381b717cc423d42ff2299e3d23ca9
Ziyi WANG <ziyiwang1027@gmail.com>
2022-06-20 14:23:56 +0200

    init app

--- pwa/index.js
+++ pwa/index.js
@@ -2,7 +2,7 @@
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+//addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -10,11 +10,11 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+    //addBtn.style.display = 'block';
 
     addBtn.addEventListener('click', () => {
         // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
+        //addBtn.style.display = 'none';
         // Show the prompt
         deferredPrompt.prompt();
         // Wait for the user to respond to the prompt
