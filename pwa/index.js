d3f4fe763ace6050e5bb74d508faea1f1bb29d13
Ziyi WANG <ziyiwang1027@gmail.com>
2022-06-20 15:41:50 +0200

    init app

--- pwa/index.js
+++ pwa/index.js
@@ -1,37 +1,45 @@
-/*
+/* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('sw.js')
-        .then(() => { console.log('Service Worker Registered'); });
+    navigator.serviceWorker.register('service-worker.js');
 }
-*/
-// Code to handle install prompt on desktop
 
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-//addBtn.style.display = 'none';
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    console.log('ok', 'beforeinstallprompt', event);
+    // 这个变量后面要用
+    window.deferredPrompt = event;
+    // 你用来显示 添加到PWA 的 按钮可以显示出来了
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+});
 
-    addBtn.addEventListener('click', () => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
+const butInstall = document.querySelector('.add-button');
+
+butInstall.addEventListener('click', () => {
+    console.log('ok', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        // deferred prompt 不可用
+        alert('deferred prompt 不可用')
+        return;
+    }
+    promptEvent.prompt();
+    promptEvent.userChoice.then((choiceResult) => {
+        console.log('ok', 'userChoice', choiceResult);
+        if (choiceResult.outcome === 'accepted') {
+            console.log('用户 同意了');
+            // 成功之后的业务回掉操作
+            // your code here
+        } else {
+            console.log('用户 没同意');
+        }
+        // 回收 deferredPrompt 变量, prompt() 只能被调用一次
+        window.deferredPrompt = null;
     });
+});
+
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+    // 回收 deferredPrompt 变量
+    window.deferredPrompt = null;
 });
